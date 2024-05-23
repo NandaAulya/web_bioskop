988450641de9a78ca5ac6751776c96a7617c625b
NandaAulya <133217309+NandaAulya@users.noreply.github.com>
2024-05-24 03:31:25 +0700

    [+} updateCinema

--- src/component/theater/cinema.php
+++ src/component/theater/cinema.php
@@ -28,17 +28,66 @@ $theaters = array(
         'location' => 'jl.walikota mustajab no.1 ketapang kec.genteng,surabaya',
         'endpoint' => ''
     ),
+    array(
+        'title' => 'ciputra world surabaya',
+        'poster' => 'https://ciputraworldsurabaya.com/mall/wp-content/uploads/2021/11/CWS-1.jpg',
+        'icon' => 'http://www.w3.org/2000/svg',
+        'location' => 'Gn. Sari, Kec. Dukuhpakis, Surabaya, Jawa Timur
+        ',
+        'endpoint' => ''
+    ),
+    array(
+        'title' => 'pakuwon city mall',
+        'poster' => 'https://www.pakuwoncity.com/wp-content/uploads/2020/11/Pakuwon-City-Mall-e1604998922874.jpg',
+        'icon' => 'http://www.w3.org/2000/svg',
+        'location' => 'Jl. Raya Laguna KJW Putih Tambak No.2, Kejawaan Putih Tamba, Kec. Mulyorejo, Surabaya,',
+        'endpoint' => ''
+    ),
+    array(
+        'title' => 'pakuwon mall surabaya',
+        'poster' => 'https://transfashionindonesia.com/wp-content/uploads/2023/11/Pakuwon-Mall-Surabaya_02.jpg',
+        'icon' => 'http://www.w3.org/2000/svg',
+        'location' => 'Jl. Mayjend. Jonosewojo No.2, Babatan, Kec. Wiyung, Surabaya',
+        'endpoint' => ''
+    ),
+    array(
+        'title' => 'marvel city mall',
+        'poster' => 'https://images.glints.com/unsafe/glints-dashboard.s3.amazonaws.com/company-banner-pic/16d94acf990a5c3283373038616f9098.jpg',
+        'icon' => 'http://www.w3.org/2000/svg',
+        'location' => 'Jl. Ngagel No.123, Ngagel, Kec. Wonokromo, Surabaya',
+        'endpoint' => ''
+    ),
+    array(
+        'title' => 'bg junction mall',
+        'poster' => 'https://r1.community.samsung.com/t5/image/serverpage/image-id/7692900iFB7C812E20681E52?v=v2',
+        'icon' => 'http://www.w3.org/2000/svg',
+        'location' => 'Jl. Bubutan No.1-7, Bubutan, Kec. Bubutan, Surabaya',
+        'endpoint' => ''
+    ),
+    array(
+        'title' => 'lenmarc mall',
+        'poster' => 'https://image.jpnn.com/resize/570x380-80/arsip/normal/2017/02/13/ee92f70da7642465396efd89f219b919.jpg',
+        'icon' => 'http://www.w3.org/2000/svg',
+        'location' => 'Jl. Mayjend. Jonosewojo No.9, Pradahkalikendal, Kec. Dukuhpakis, Surabaya,',
+        'endpoint' => ''
+    ),
 );
 ?>
-
-<div class="flex flex-row gap-6 ml-10 mt-10 mx-auto drop-shadow-auto">
+<div class="flex flex-row flex-wrap gap-6 ml-10 mt-10 mx-auto drop-shadow-auto ">
     <?php foreach ($theaters as $theater): ?>
-        <div class="shadow-2xl overflow-hidden rounded-md shadow transition hover:shadow-lg" style="width: 300px;">
+        <div class="shadow-2xl overflow-hidden rounded-md shadow transition hover:shadow-lg mb-4" style="width: 300px;">
             <img src="<?php echo $theater['poster']; ?>" class="h-[150px] w-[300px] object-cover">
             <div class="bg-white-200 p-2 sm:p-4">
-                <h3 class="capitalize text-gray-800 text-xl font-bold hover:text-gray-700 transition"><?php echo $theater['title']; ?></h3>
+                <a href="#">
+                    <h3 class="capitalize text-gray-600 text-xl font-bold hover:text-gray-500 transition">
+                        <?php echo $theater['title']; ?>
+                    </h3>
+                </a>
                 <div class="flex items-center">
-                <i class="fa-solid fa-location-dot"></i>
+                    <i class="fa-solid fa-location-dot"></i>
+                    <div class="ml-4 mt-3">
+                        <p class="capitalize text-sm"><?php echo $theater['location']; ?></p>
+                    </div>
                 </div>
             </div>
         </div>
