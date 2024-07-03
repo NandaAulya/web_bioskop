cee1a5ae749e3342771678695d2d056b2984f3f1
NandaAulya <nandhakampir@gmail.com>
2024-07-04 03:41:11 +0700

    [+]login

--- auth/login.php
+++ auth/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php
-    $title = "Register";
+    $title = "Login";
     include "../config/config.php";
     include "../controller/controllerUser.php";
 
